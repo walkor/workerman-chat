c1e130b1ad8ab5192d5d40e18992b4e644d6625b
walkor <worker-man@qq.com>
2014-04-10 00:27:57 +0800

    ci

--- applications/Chat/Web/index.php
+++ applications/Chat/Web/index.php
@@ -19,7 +19,7 @@
     var ws, name, user_list={};
     function init() {
        // 创建websocket
-      ws = new WebSocket("ws://<?php echo $_SERVER['SERVER_ADDR']?>:7272/");
+    	ws = new WebSocket("ws://"+document.domain+":7272/");
       // 当socket连接打开时，输入用户名
       ws.onopen = function() {
     	  show_prompt();
