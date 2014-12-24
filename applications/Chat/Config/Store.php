6f2077406080a57abe65f4ad8aa6b7eb13bbc647
walkor <worker-man@qq.com>
2014-12-25 01:39:49 +0800

    path

--- applications/Chat/Config/Store.php
+++ applications/Chat/Config/Store.php
@@ -27,9 +27,11 @@ class Store
         '127.0.0.1:22301',
     );
     
-    /* 如果使用文件存储，则在这里设置数据存储的目录，默认/tmp/下
-     * 注意：如果修改了storePath，要将storePath加入到conf/conf.d/FileMonitor.conf的忽略目录中 
-     * 例如 $storePath = '/home/data/',则需要在conf/conf.d/FileMonitor.conf加一行 exclude_path[]=/home/data/
+    /*
+     *  如果使用文件存储，默认系统临时目录下
      */
-    public static $storePath = './logs/workerman-chat/';
+    public static $storePath = '';
 }
+
+// 默认系统临时目录下
+Store::$storePath = sys_get_temp_dir().'/workerman-chat/';
