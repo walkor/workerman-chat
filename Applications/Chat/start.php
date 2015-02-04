35eb14c358eafbf8a714ac301d0872bad1915e46
walkor <walkor@workerman.net>
2015-02-04 22:14:20 +0800

    update to version 3.0

---
+++ Applications/Chat/start.php
@@ -0,0 +1,35 @@
+<?php 
+use \Workerman\WebServer;
+use \GatewayWorker\Gateway;
+use \GatewayWorker\BusinessWorker;
+
+// gateway
+$gateway = new Gateway("Websocket://0.0.0.0:7272");
+
+$gateway->name = 'ChatGateway';
+
+$gateway->count = 4;
+
+$gateway->lanIp = '127.0.0.1';
+
+$gateway->startPort = 1000;
+
+$gateway->pingInterval = 10;
+
+$gateway->pingData = '{"type":"ping"}';
+
+
+// bussinessWorker
+$worker = new BusinessWorker();
+
+$worker->name = 'ChatBusinessWorker';
+
+$worker->count = 4;
+
+
+// WebServer
+$web = new WebServer("http://0.0.0.0:55151");
+
+$web->count = 2;
+
+$web->addRoot('www.workerman.net', __DIR__.'/Web');
