c2e073b526f9d481a4d458ea698783fab41bf6c0
walkor <worker-man@qq.com>
2015-03-17 22:21:55 +0800

    update workerman core to 3.0.8

--- Applications/Chat/start.php
+++ Applications/Chat/start.php
@@ -1,21 +1,26 @@
 <?php 
+use \Workerman\Worker;
 use \Workerman\WebServer;
 use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
 
-// gateway
+// 自动加载类
+require_once __DIR__ . '/../../Workerman/Autoloader.php';
+
+// gateway 进程
 $gateway = new Gateway("Websocket://0.0.0.0:7272");
-
+// 设置名称，方便status时查看
 $gateway->name = 'ChatGateway';
-
+// 设置进程数，gateway进程数建议与cpu核数相同
 $gateway->count = 4;
-
+// 分布式部署时请设置成内网ip（非127.0.0.1）
 $gateway->lanIp = '127.0.0.1';
-
+// 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
+// 则一般会使用4001 4002 4003 4004 4个端口作为内部通讯端口 
 $gateway->startPort = 3000;
-
+// 心跳间隔
 $gateway->pingInterval = 10;
-
+// 心跳数据
 $gateway->pingData = '{"type":"ping"}';
 
 /* 
@@ -37,17 +42,25 @@ $gateway->onConnect = function($connection)
 */
 
 
-// bussinessWorker
+// bussinessWorker 进程
 $worker = new BusinessWorker();
-
-$worker->name = 'ChatBusinessWorker';
-
+// worker名称
+$worker->name = 'TodpoleBusinessWorker';
+// bussinessWorker进程数量
 $worker->count = 4;
 
 
 // WebServer
 $web = new WebServer("http://0.0.0.0:55151");
-
+// WebServer数量
 $web->count = 2;
+// 设置站点根目录
+$web->addRoot('www.your_domain.com', __DIR__.'/Web');
 
-$web->addRoot('www.workerman.net', __DIR__.'/Web');
+
+// 如果不是在根目录启动，则运行runAll方法
+if(!defined('GLOBAL_START'))
+{
+    Worker::runAll();
+}
+
