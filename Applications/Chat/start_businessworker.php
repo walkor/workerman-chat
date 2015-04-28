83c8eeb958fed54e208b14d265ea1b0fa251254c
walkor <worker-man@qq.com>
2015-04-28 23:57:47 +0800

    start_**

---
+++ Applications/Chat/start_businessworker.php
@@ -0,0 +1,24 @@
+<?php 
+use \Workerman\Worker;
+use \Workerman\WebServer;
+use \GatewayWorker\Gateway;
+use \GatewayWorker\BusinessWorker;
+use \Workerman\Autoloader;
+
+// 自动加载类
+require_once __DIR__ . '/../../Workerman/Autoloader.php';
+Autoloader::setRootPath(__DIR__);
+
+// bussinessWorker 进程
+$worker = new BusinessWorker();
+// worker名称
+$worker->name = 'ChatBusinessWorker';
+// bussinessWorker进程数量
+$worker->count = 4;
+
+// 如果不是在根目录启动，则运行runAll方法
+if(!defined('GLOBAL_START'))
+{
+    Worker::runAll();
+}
+
---
+++ Applications/Chat/start_web.php
@@ -0,0 +1,25 @@
+<?php 
+use \Workerman\Worker;
+use \Workerman\WebServer;
+use \GatewayWorker\Gateway;
+use \GatewayWorker\BusinessWorker;
+use \Workerman\Autoloader;
+
+// 自动加载类
+require_once __DIR__ . '/../../Workerman/Autoloader.php';
+Autoloader::setRootPath(__DIR__);
+
+// WebServer
+$web = new WebServer("http://0.0.0.0:55151");
+// WebServer数量
+$web->count = 2;
+// 设置站点根目录
+$web->addRoot('www.your_domain.com', __DIR__.'/Web');
+
+
+// 如果不是在根目录启动，则运行runAll方法
+if(!defined('GLOBAL_START'))
+{
+    Worker::runAll();
+}
+
