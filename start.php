13b5520b2d9b4cf3dd29dd88c11a9b6bf1c1810e
walkor <walkor@users.noreply.github.com>
2016-09-06 23:02:30 +0800

    Update start.php

--- start.php
+++ start.php
@@ -15,12 +15,12 @@ if(strpos(strtolower(PHP_OS), 'win') === 0)
 // 检查扩展
 if(!extension_loaded('pcntl'))
 {
-    exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
+    exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
 
 if(!extension_loaded('posix'))
 {
-    exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
+    exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
 
 // 标记是全局启动
