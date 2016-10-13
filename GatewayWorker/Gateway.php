3cfcbdd41883d73f315910d19d5c5f6b077cdd75
walkor <walkor@users.noreply.github.com>
2016-10-13 18:39:13 +0800

    Update Gateway.php

--- GatewayWorker/Gateway.php
+++ GatewayWorker/Gateway.php
@@ -343,7 +343,7 @@ class Gateway extends Worker
             /** @var TcpConnection $worker_connection */
             $worker_connection = call_user_func($this->router, $this->_workerConnections, $connection, $cmd, $body);
             if (false === $worker_connection->send($gateway_data)) {
-                $msg = "SendBufferToWorker fail. May be the send buffer are overflow";
+                $msg = "SendBufferToWorker fail. May be the send buffer are overflow. See http://wiki.workerman.net/Error2";
                 $this->log($msg);
                 return false;
             }
@@ -353,7 +353,7 @@ class Gateway extends Worker
             // 所以不记录日志，只是关闭连接
             $time_diff = 2;
             if (time() - $this->_startTime >= $time_diff) {
-                $msg = 'SendBufferToWorker fail. The connections between Gateway and BusinessWorker are not ready';
+                $msg = 'SendBufferToWorker fail. The connections between Gateway and BusinessWorker are not ready. http://wiki.workerman.net/Error3';
                 $this->log($msg);
             }
             $connection->destroy();
