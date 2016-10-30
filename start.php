2493860c9d0595d413ce8866f1476a9006963859
walkor <walkor@workerman.net>
2016-10-30 21:06:38 +0800

    vendor/autoload.php

--- start.php
+++ start.php
@@ -26,7 +26,7 @@ if(!extension_loaded('posix'))
 // 标记是全局启动
 define('GLOBAL_START', 1);
 
-require_once __DIR__ . '/Workerman/Autoloader.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
 foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
