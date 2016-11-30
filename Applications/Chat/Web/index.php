c3ddb394966ba027fbc6621394a5184788227491
walkor <walkor@users.noreply.github.com>
2016-11-30 17:11:31 +0800

    Update index.php

--- Applications/Chat/Web/index.php
+++ Applications/Chat/Web/index.php
@@ -1,12 +1,9 @@
 <html><head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>workerman-chat PHP聊天室 Websocket(HTLM5/Flash)+PHP多进程socket实时推送技术</title>
-  <script type="text/javascript">
-  //WebSocket = null;
-  </script>
   <link href="/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/style.css" rel="stylesheet">
-  <!-- Include these three JS files: -->
+	
   <script type="text/javascript" src="/js/swfobject.js"></script>
   <script type="text/javascript" src="/js/web_socket.js"></script>
   <script type="text/javascript" src="/js/jquery.min.js"></script>
