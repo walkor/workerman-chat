127ff216b0fbe0020af47f1ed8836a55705835d4
walkor <walkor@users.noreply.github.com>
2018-03-16 17:47:45 +0800

    Update start_register.php

--- Applications/Chat/start_register.php
+++ Applications/Chat/start_register.php
@@ -14,6 +14,8 @@
 use \Workerman\Worker;
 use \GatewayWorker\Register;
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 // register 服务必须是text协议
 $register = new Register('text://0.0.0.0:1236');
 
