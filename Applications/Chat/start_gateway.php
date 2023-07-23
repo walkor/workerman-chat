9e4ef7fb907f4a8b1f03eeb10a4b771ff6f6fa2f
walkor <walkor@workerman.net>
2023-07-23 16:54:49 +0800

    ave

--- Applications/Chat/start_gateway.php
+++ Applications/Chat/start_gateway.php
@@ -21,12 +21,12 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $gateway = new Gateway("Websocket://0.0.0.0:7272");
 // 设置名称，方便status时查看
 $gateway->name = 'ChatGateway';
-// 设置进程数，gateway进程数建议与cpu核数相同
-$gateway->count = 4;
+// 设置进程数，一般两个进程就足够
+$gateway->count = 2;
 // 分布式部署时请设置成内网ip（非127.0.0.1）
 $gateway->lanIp = '127.0.0.1';
-// 内部通讯起始端口。假如$gateway->count=4，起始端口为2300
-// 则一般会使用2300 2301 2302 2303 4个端口作为内部通讯端口 
+// 内部通讯起始端口。假如$gateway->count=2，起始端口为2300
+// 则一般会使用2300 2301 2个端口作为内部通讯端口
 $gateway->startPort = 2300;
 // 心跳间隔
 $gateway->pingInterval = 10;
--- Applications/Chat/start_register.php
+++ Applications/Chat/start_register.php
@@ -16,7 +16,8 @@ use \GatewayWorker\Register;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-// register 服务必须是text协议，监听地址请用内网ip或者127.0.0.1。register服务不能被外网访问。
+// register服务必须是text协议，监听地址请用内网ip或者127.0.0.1
+// 为了安全，register不能监听0.0.0.0，也就是register服务不能暴露给外网
 $register = new Register('text://127.0.0.1:1236');
 
 // 如果不是在根目录启动，则运行runAll方法
